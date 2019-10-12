1dbcbb0111293f02ebf9f80a469a0af1479d2576
keith-mykmi <keith@mykmi.co.uk>
2019-10-12 22:10:15 +0100

    First commit

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
+#!/usr/bin/python3
+
+
 from flask import Flask
+import pymongo
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+
+    uri = r"mongodb://wtl:ThC65b3JBYd1CaCYqPaNImOFb6fWj4wOFeH1eanTWhaM4MqaYdq6kaCMQXf6dQU8YQTYRUaUjshiVp59eHoFsw==@wtl.documents.azure.com:10255/?ssl=true&replicaSet=globaldb"
+    myclient = pymongo.MongoClient(uri)
+
+    mydb = myclient["mydatabase"]
+    mycol = mydb["customers"]
+    
+    x= mycol.find_one()
+
+
+    return "Hello World: "+x['name']
